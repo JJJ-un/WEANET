f277b17dbee97bcb0dd55bf26ef337f55003650f
JJJ-un <jhjk7272@naver.com>
2025-12-06 18:58:20 +0900

    chore: 디자인 시스템 단위 변경 HSL -> HEX

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@/shared/ui/button";
+import { Input } from "@/shared/ui/input";
+import * as S from "@/shared/ui/card";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <Button>시작하기</Button>;
+  return (
+    <div>
+      <Button>시작하기</Button>
+      <div>구분선</div>
+      <S.Card className="w-[400px]">
+        <S.CardHeader>
+          <S.CardTitle>카드 제목</S.CardTitle>
+          <S.CardDescription>카드 설명</S.CardDescription>
+        </S.CardHeader>
+        <S.CardContent>
+          <Input placeholder="입력하세요" />
+        </S.CardContent>
+        <S.CardFooter>카드 푸터</S.CardFooter>
+      </S.Card>
+    </div>
+  );
 }
