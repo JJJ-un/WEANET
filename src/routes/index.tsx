bdbfc590dba958e7695fa190ce9aa12ef0bdffa0
JJJ-un <jhjk7272@naver.com>
2025-12-08 21:00:24 +0900

    feat: Daily-Forecast UI 구현

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import CurrentWeather from "@/widget/current-weather/ui/CurrentWeather";
+import DailyForecast from "@/widget/current-weather/ui/DailyForecast";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -7,8 +8,9 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <CurrentWeather />
+      <DailyForecast />
     </div>
   );
 }
