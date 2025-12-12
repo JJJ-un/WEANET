454357de7812a51e06af9806e6b190ff9f9ed610
JJJ-un <jhjk7272@naver.com>
2025-12-12 17:47:55 +0900

    feat: 현재 날씨 컴포넌트 UI 퍼블리싱

--- src/widget/current-weather/ui/CurrentWeather.tsx
+++ src/widget/current-weather/ui/CurrentWeather.tsx
@@ -4,12 +4,14 @@ import WeatherIcon from "@/shared/assets/icons/weather.svg?react";
 const CurrentWeather = () => {
   // wiget은 피그마 ui 컴포넌트와 제일 가깝나?
   return (
-    <S.Card className="w-[400px] h-[91px] flex justify-center items-center gap-8">
+    <S.Card className="w-[100%] h-[91px] flex justify-center items-center gap-8">
       <S.CardContent className="flex justify-between items-center w-full">
-        <WeatherIcon />
-        <div>
-          <S.CardTitle>강남구 현재 날씨</S.CardTitle>
-          <S.CardDescription>맑음, 25°C</S.CardDescription>
+        <div className="flex items-center gap-6">
+          <WeatherIcon />
+          <div className="flex flex-col gap-1">
+            <S.CardDescription>강남구 현재 날씨</S.CardDescription>
+            <S.CardTitle className="text-xl">맑음, 25°C</S.CardTitle>
+          </div>
         </div>
         <div>
           <S.CardDescription>강수 10%</S.CardDescription>
