7452649033d598ad93bc4a4871c8e4175759b69e
JJJ-un <jhjk7272@naver.com>
2025-12-12 13:17:42 +0900

    chore: main.tsx 파일 prettier에 따른 코드 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,43 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { QueryClient } from "@tanstack/react-query";
-import { routeTree } from "./routeTree.gen";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+import { routeTree } from './routeTree.gen';
+import './index.css';
 
 export const queryClient = new QueryClient();
 
 const router = createRouter({
-  routeTree,
-  context: {
-    queryClient,
-  },
+    routeTree,
+    context: {
+        queryClient,
+    },
 });
 
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
 }
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("SW 등록 성공:", registration);
-      })
-      .catch((error) => {
-        console.log("SW 등록 실패:", error);
-      });
-  });
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log('SW 등록 성공:', registration);
+            })
+            .catch((error) => {
+                console.log('SW 등록 실패:', error);
+            });
+    });
 }
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>,
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </StrictMode>,
 );
