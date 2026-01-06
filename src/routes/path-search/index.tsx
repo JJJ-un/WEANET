5c27f4a45cb82f29740cc4a85f21579e0a204334
JJJ-un <jhjk7272@naver.com>
2026-01-06 14:48:07 +0900

    feat: 경로탐색 페이지의 기본 레이아웃 구성

--- src/routes/path-search/index.tsx
+++ src/routes/path-search/index.tsx
@@ -1,9 +1,25 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/path-search/")({
-  component: RouteComponent,
+  component: PathSearchPage,
 });
 
-function RouteComponent() {
-  return <div>Hello</div>;
+function PathSearchPage() {
+  return (
+    <div>
+      {/* 헤더 컴포넌트 */}
+      <div>
+        <div>아이콘</div>
+        <h1>경로 검색</h1>
+      </div>
+      {/* 검색 바 및 최근 검색어 컴포넌트 */}
+      <div>
+        <div>검색 바 컴포넌트</div>
+      </div>
+      {/* 최근 검색어 컴포넌트 */}
+      <div>
+        <div>최근 검색어 컴포넌트</div>
+      </div>
+    </div>
+  );
 }
