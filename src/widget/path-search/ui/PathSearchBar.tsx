9f4b4b48b0a967e241f4b2cc066ea6fcd07b6921
JJJ-un <jhjk7272@naver.com>
2026-01-16 00:04:48 +0900

    feat: 포커싱 시 검색어 목록 노출

--- src/widget/path-search/ui/PathSearchBar.tsx
+++ src/widget/path-search/ui/PathSearchBar.tsx
@@ -1,23 +1,40 @@
 import { Input } from "@/shared/ui/input";
+import { useState } from "react";
 import DestinationIcon from "@/shared/assets/icons/destination.svg?react";
 import DepartureIcon from "@/shared/assets/icons/departure.svg?react";
 
 const PathsSearchBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex flex-col items-center gap-4 p-4 bg-background border-2 rounded-[10px]">
       <div className="flex items-center gap-2">
         <DepartureIcon />
         <div>
           <p className="text-sm">출발지</p>
-          <Input />
+          <Input
+            onFocus={() => setIsOpen(true)}
+            onBlur={() => setIsOpen(false)}
+          />
         </div>
       </div>
       <div className="flex items-center gap-2">
         <DestinationIcon />
         <div>
           <p>도착지</p>
-          <Input placeholder="도착위치를 입력하세요" />
+          <Input
+            placeholder="도착위치를 입력하세요"
+            onFocus={() => setIsOpen(true)}
+            onBlur={() => setIsOpen(false)}
+          />
         </div>
+      </div>
+      <div>
+        {isOpen && (
+          <div className="w-full h-40 bg-white border mt-2 rounded-lg">
+            검색 결과 영역
+          </div>
+        )}
       </div>
     </div>
   );
