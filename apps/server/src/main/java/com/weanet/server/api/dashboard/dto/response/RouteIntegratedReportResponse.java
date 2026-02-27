6d3724bebbd3d1716e26248009107cf11d755259
JJJ-un <jhjk7272@naver.com>
2026-02-27 23:19:40 +0900

    refactor: RouteIntegratedReportResponse를 대시보드 도메인으로 이동

--- apps/server/src/main/java/com/weanet/server/api/dashboard/dto/response/RouteIntegratedReportResponse.java
+++ apps/server/src/main/java/com/weanet/server/api/dashboard/dto/response/RouteIntegratedReportResponse.java
@@ -1,4 +1,4 @@
-package com.weanet.server.api.route.dto.response;
+package com.weanet.server.api.dashboard.dto.response;
 
 import com.weanet.server.api.report.dto.ReportResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
